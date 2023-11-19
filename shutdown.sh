549b4140c896432a4397700f3bbf3b494e15123c
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-11-19 13:17:53 +0530

    Add shutdown util for computer when pinged

---
+++ shutdown.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# shut down computer when pinged, if locked, from any home device
+# intent: preserve monitor from overheating
+# setup: add as computer startup script
+# works on macos, linux
+function startShutdownServer() {
+    jump home-controller
+    PORT=4001 node app.js
+    cd -
+}
