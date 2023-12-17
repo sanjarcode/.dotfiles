84f6a4c8fcf54837b4949be2b23dbf1e5fbfa56a
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-12-18 02:02:09 +0530

    Add home server run on computer startup. safe, just swtiches

--- shutdown.sh
+++ shutdown.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+PORT=4001
+
 # shut down computer when pinged, if locked, from any home device
 # intent: preserve monitor from overheating
 # setup: add as computer startup script
@@ -7,5 +9,19 @@
 function startShutdownServer() {
     hc_path=$(getmark hc)
     hc_script_path="$hc_path/app.js"
-    PORT=4001 node $hc_script_path
+    PORT="${PORT}" node $hc_script_path
 }
+
+function startShutdownServerIdempotent() {
+    # Check if localhost:4001 is reachable
+    isServerRunning=$(curl -sSf "http://localhost:${PORT}" > /dev/null 2>&1)
+    if [ $? != 0 ]; then
+        startShutdownServer > /dev/null 2>&1 &
+    else
+        # Echo a message indicating the server is already running
+        echo "Server already running on localhost:${PORT}"
+    fi
+}
+
+# Add to .zshrc
+# $(startShutdownServerIdempotent > /dev/null 2>&1 &)
