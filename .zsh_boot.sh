1397fdacf5a92892d0e40fa94a4c02cfe5accd04
Sanjar Afaq <sanjarcode@gmail.com>
2025-09-24 16:34:03 +0530

    fix update

--- .zsh_boot.sh
+++ .zsh_boot.sh
@@ -3,7 +3,7 @@
 # Helps catch browser links to call
 function setup_dialer() {
     node terminal-to-web-proxy &> /dev/null &
-    adb start-server &> /dev/null &
+    adb devices &> /dev/null &
 }
 
 setup_dialer
